34c5c12e915e453d0aa18a034bbe140690bc68dc
aaravmody <aaravmody2@gmail.com>
2024-03-01 18:49:31 +0530

    Time problem solved

--- utils/messages.js
+++ utils/messages.js
@@ -1,9 +1,22 @@
-const moment=require('moment');
+// const moment=require('moment');
 
-function formatmsg(username,text){
-    return{
-        username,text,time:moment().format('h:mm a')
-    }
+// function formatmsg(username,text){
+//     return{
+//         username,text,time:moment().format('h:mm a')
+//     }
+// }
+
+// module.exports = formatmsg;
+
+function formatmsg(username, text) {
+    const currentTime = new Date();
+    const formattedTime = `${currentTime.getHours() > 12 ? currentTime.getHours() - 12 : currentTime.getHours()  }:${currentTime.getMinutes()} ${currentTime.getHours() >= 12 ? 'PM' : 'AM'}`;
+    
+    return {
+        username,
+        text,
+        time: formattedTime
+    };
 }
 
 module.exports = formatmsg;
